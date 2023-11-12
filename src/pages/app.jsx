82b0ae3cb42dbc8ac3618d790ba3fb2e67b77bdb
Andrea Carmona <andrea@carmonaweb.com>
2023-11-12 17:15:43 -0500

    Finish dashboard3

---
+++ src/pages/app.jsx
@@ -0,0 +1,13 @@
+import Layout from "../template/layout"
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
